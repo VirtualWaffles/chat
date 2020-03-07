20be01b20aaf2f061e45eb99b9917fae9f21e9c2
VIrtualWaffles <jshew085@myroyal.ca>
2020-03-06 23:24:30 -0700

    Oh right... I did a lot and forgot to commit

--- client/chat.js
+++ client/chat.js
@@ -1,25 +1,100 @@
+"use strict";
+
 $(document).ready(function(){
     console.log("Script loaded");
     let socket = io(); //Initialize client
-    
+    let username;
+    let color = '000000';
+
     //on form submitted or button clicked event handler
     $('form').submit(function(e){
       console.log('message entered');
-
       e.preventDefault(); //Prevents page reloading
-      socket.emit('message', $('#m').val()); //Send message to server by sending the message as an event.
-      $('#m').val(''); //Clear message box
+      let text = $('#m').val();
+
+      //ignore empty input
+      if(!text)
+        return false;
+
+      //change color
+      else if(text.startsWith("/nickcolor"))
+        is_valid_color(text.slice(11)) ? color = text.slice(11) : err('color');
+
+      //change name
+      else if(text.startsWith("/nick"))
+        socket.emit('nickname', {old: username, new: text.slice(6)})
+
+      //send message
+      else
+        socket.emit('message', {user: username, text: text, color: color});
+      
+      //clear message box
+      $('#m').val('');        
       return false;
     });
     
+
+
     //'chat message' event listener.
     socket.on('message', function(msg){
       console.log('message received')
-      
+      add_message(msg);
+    });
+
+
+    
+    //when a client joins update their log of users and messages and
+    //give them a username
+    socket.on('welcome', function(data){
+      username = data['name'];
+      console.log('welcome ' + username);
+      // $('#username').text(data['name']);
+      $('#m').attr('placeholder', 'Enter your message ' + username);
+      for(const user of data['users'])
+        add_user(user);
+    });
+
+
+
+    //add users when they join
+    socket.on('user-join', function(user){
+      console.log('user "' + user + '" joined');
+      add_user(user);
+    });
+
+
+
+    //remove users when they leave
+    socket.on('user-left', function(name){
+      console.log('user "' + name + '" left');
+      for(const user of $('.user')){
+        if($(user).text() === name){
+          $(user).parent().remove();
+          break;
+        }
+      }
+    });
+
+
+    
+    //alert user when name is taken
+    socket.on('name-taken', function(names){
+      err('name');
+    });
+
+
+    function add_user(name){
+      let li = $('<li class="list-group-item text-center"></li>');
+      let usr = $('<label class="user">' + name + '</label>');
+      li.append(usr);
+      $('#users').append(li);       
+    };
+
+    function add_message(message){
       let li = $('<li class="list-group-item"></li>');
-      let usr = $('<label class="username">' + msg['user'] + '</label>');
-      let tim = $('<label class="timestamp">' + msg['time'] + '</label>');
-      let txt = $('<p class="message">' + msg['text'] + '</p>');
+      let usr = $('<label class="username" style="color: #' + message['color'] + ';">' + message['user'] + '</label>');
+      let tim = $('<label class="timestamp">' + message['time'] + '</label>');
+      let txt = $('<p class="message">' + message['text'] + '</p>');
       
       li.append(usr);
       li.append(tim);
@@ -27,5 +102,32 @@ $(document).ready(function(){
 
       $('#messages').append(li);
       $('#messages').scrollTop($('#messages').prop("scrollHeight"));
-    });
+    };
+
+
+    //checks if text is a valid hex color
+    function is_valid_color(text){
+      //https://stackoverflow.com/questions/8027423/how-to-check-if-a-string-is-a-valid-hex-color-representation/8027444
+      console.log('Testing color ' + text);
+      return(/^[0-9A-F]{6}$/i.test(text));
+    };
+
+
+    //displays error messages to the user
+    function err(type){
+      let err_message = {
+        user: 'Alert',
+        color: 'ee5502',
+        time: 'Only you can view this message',
+      }
+      if(type === 'color')
+        err_message['text'] = "Invalid color selection. Format should be /nickcolor RRGGBB."
+      else if(type === 'name')
+        err_message['text'] = "Invalid name choice. That name is already taken."
+      else
+        err_message['text'] = "An unknown error occured."
+      add_message(err_message);
+    };
+    
+
 });
--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+//https://socket.io/docs/emit-cheatsheet/
+
+"use strict";
+
 let express = require('express');
 let app = express();
 let http = require('http').createServer(app);
 let io = require('socket.io')(http);
 let moment = require('moment');
+
+let users = [];
+let messages = [];
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
@@ -16,19 +23,38 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   console.log('User connected');
+  let user = 'User' + Math.floor(Math.random() * 1001);
+  socket.user = user;
+
+  users.push(user);
+
+  let data = {
+    name: user,
+    users: users,
+    messages: {},
+  }
+
+  io.to(socket.id).emit('welcome', data);
+  // socket.send('welcome', data);
+  socket.broadcast.emit('user-join', user);
 
   socket.on('disconnect', function(){
-    console.log('User disconnected');
+    console.log('User "' + socket.user + '" disconnected'); 
+    users = users.filter(u => u !== socket.user);
+    io.emit('user-left', socket.user);
   });
 
-  socket.on('message', function(t){
-    console.log('Message: ' + t);
-    let msg = {
-      user: 'test',
-      text: t,
-      time: moment().format("DD/MM/YYYY h:m A"),
-    }
+  socket.on('message', function(msg){
+    console.log('Message: ' + msg['text']);
+    msg['time'] = moment().format("DD/MM/YYYY h:m A");
     io.emit('message', msg);
+  });
+
+  socket.on('nickname', function(names){
+    console.log(names['old'] + ' wants to be ' + names['new']);
+    if(users.includes(names['new']))
+      io.to(socket.id).emit('name-taken', names);
+
   });
 
 });
